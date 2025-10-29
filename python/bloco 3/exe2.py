80b66b4420612bab7de557d3f1693b24da97d9a0
Mabss <mabssdugrauu07@gmail.com>
2025-10-29 11:51:01 -0300

    Delete python/bloco 3 directory

--- python/bloco 3/exe2.py
+++
@@ -1,8 +0,0 @@
-num = (float(input('Digite um número: ')))
-
-if num > 0:
-    print('Parabéns, seu número é positivo!')
-elif num< 0:
-    print("Seu número é negativo. Tente novamente...")
-else:
-    print("O número é zero.")
--- python/bloco 3/exe4.py
+++
@@ -1,6 +0,0 @@
-temperatura = 23
-
-if temperatura > 30:
-    print('Está quente ein!')
-else:
-    print('Está um clima agradável.')
