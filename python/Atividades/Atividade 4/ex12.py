9d612230e1be606a3fb4fce0643e396519a76607
Mabss <mabssdugrauu07@gmail.com>
2025-11-13 11:28:57 -0300

    Segundo Commit

--- python/Atividades/Atividade 4/ex12.py
+++ python/Atividades/Atividade 4/ex12.py
@@ -1,3 +1,7 @@
 valorHora = float(input('Digite qual o valor da sua hora de trabalho: '))
 horasTrabalhadas = float(input('Digite o seu total de horas trabalhadas: '))
 
+salarioBruto = horasTrabalhadas * valorHora
+
+if salarioBruto <= 900:
+    print(f'Sálario Bruto: ({valorHora} * {horasTrabalhadas}): R${salarioBruto:.2f} \n(-)IR: ISENTO')
